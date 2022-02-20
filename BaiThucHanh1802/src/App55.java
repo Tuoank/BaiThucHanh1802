ec55dfa5524b9f21325b9c40706a1ae1e10d9536
Tuong Anh <98013554+Tuoank@users.noreply.github.com>
2022-02-20 21:09:55 +0700

    ok

---
+++ BaiThucHanh1802/src/App55.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class App55 {
+    public static void main(String args[]) throws Exception {
+        for(int i=0;i<=20;i++){
+        if(i % 5 == 0){
+        break;   
+    }
+    System.out.println("Cac so chia het cho 5 la: "+i);
+   } 
+ }
+}
